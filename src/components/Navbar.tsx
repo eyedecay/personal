5d7f746c9c341f5ce8e5a983a61be248812b8cc0
johnathan <notbuffalo09@gmail.com>
2026-07-02 22:35:15 -0400

    formatting navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,14 @@ import ModeToggle from "./ModeToggle.tsx"
 export default function NavBar() {
     return (
         <nav className = "navbar">
+            <ModeToggle/>
             <ul className = "links">
                 <li> <a href = "#about"> About </a> </li>
                 <li> <a href = "#projects"> Projects </a> </li>
                 <li> <a href = "#contact"> Contact </a> </li>
             </ul>
 
-            <ModeToggle/>
+            
         </nav>
     )
 }
